355744d59fcbb29cdab8e4e25b1088289d66b131
iamrubydevi <ariesabyss@gmail.com>
2023-09-05 18:48:58 +0530

    Fix Linters errors

--- student.rb
+++ student.rb
@@ -4,9 +4,9 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom, age, name = 'Unknown', parents_permission: true)
-    super(age, name = name, parents_permission: parents_permission)
+    super(age, name, parents_permission: parents_permission)
     @classroom = classroom
-    classroom.add_student(self) if classroom
+    classroom&.add_student(self)
   end
 
   def play_hooky
