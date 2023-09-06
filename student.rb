a3c5eb7cc97b06a343ac60e48a147642c314f5f7
iamrubydevi <ariesabyss@gmail.com>
2023-09-06 11:34:12 +0530

    Add a separate method instead

--- student.rb
+++ student.rb
@@ -6,10 +6,14 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parents_permission: true)
     super(age, name, parents_permission: parents_permission)
     @classroom = classroom
-    classroom&.add_student(self)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def assign_classroom(classroom)
+    self.classroom = classroom
+    classroom&.add_student(self)
   end
 end
