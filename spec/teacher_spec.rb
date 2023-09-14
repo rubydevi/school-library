5f5830137fc7e7a3d537ebfe8883078f0201788d
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-09-14 12:31:14 +0530

    add teacher_spec

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,7 +1,22 @@
 require_relative '../app_code/teacher'
 
 describe Teacher do
-  before :each do
-    @teacher = Teacher.new('Math')
+  let(:teacher) { Teacher.new(36, 'Maths', 'Supreeti', parents_permission: true) }
+  let(:specialization) { double('Specialization') } # Creating a mock specialization
+
+  describe '#initialize' do
+    it 'creates a new Teacher instance with the given attributes' do
+      expect(teacher.age).to eq(36)
+      expect(teacher.specialization).to eq('Maths')
+      expect(teacher.name).to eq('Supreeti')
+      expect(teacher.parents_permission).to be_truthy
+      expect(teacher.type).to eq('Teacher')
+    end
+  end
+
+  describe '#can_use_services?' do
+    it 'returns true' do
+      expect(teacher.can_use_services?).to eq(true)
+    end
   end
 end
